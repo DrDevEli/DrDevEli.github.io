47a42439db48c8f46894a68286ebd262caec2eea
Milli <117689938+DrDevEli@users.noreply.github.com>
2025-02-16 17:52:50 +0100

    HTML img event listener test

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const projectHTML = document.getElementById('projectsHTML');
+const projectCSS = document.getElementById('projectsCSS');
+const projectJS = document.getElementById('projectsJS');
+
+projectHTML.addEventListener('click', function(){
+    alert('Button clicked')
+})

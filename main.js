321a40a183bf2ff8d166822b2ed552b61c1f09fe
Milli <117689938+DrDevEli@users.noreply.github.com>
2025-02-16 18:26:57 +0100

    fix typeError on eventListener

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 //Adding event listeners to the img from Portfolio 
-const projectHTML = document.getElementById('projectsHTML');
+const projectHTML = document.getElementById('projectsHTML').addEventListener('click', function(){
+    // Redirect to a different page
+    window.location.href = 'https://github.com/DrDevEli/HTML_Basics'; // Redirect to Git repo
+ });;
 const projectCSS = document.getElementById('projectsCSS');
 const projectJS = document.getElementById('projectsJS');
 //Creating an event listener 
-projectHTML.addEventListener('click', function(){
-   // Redirect to a different page
-   window.location.href = 'https://github.com/DrDevEli/HTML_Basics'; // Redirect to Git repo
-});

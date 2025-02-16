0806a8f7270e893bc31bb5ac651404007811a7e8
Milli <117689938+DrDevEli@users.noreply.github.com>
2025-02-16 22:47:52 +0100

    test

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const projectHTML = document.getElementById('projectsHTML');
 const projectCSS = document.getElementById('projectsCSS');
 const projectJS = document.getElementById('projectsJS');
 //Creating an event listener 
-projectHTML.onclick( function(){
+projectHTML.addEventListener( function(){
     console.log(projectHTML);
    // Redirect to a different page
    //window.open('https://github.com/DrDevEli/HTML_Basics', '_blank'); // Opens in a new tab Git repo

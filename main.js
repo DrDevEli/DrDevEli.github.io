76b72e2c933ae510af080fb0f6bb1702d9389aac
Milli <117689938+DrDevEli@users.noreply.github.com>
2025-02-16 18:01:11 +0100

    html img event listener

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-
+//Adding event listeners to the img from Portfolio 
 const projectHTML = document.getElementById('projectsHTML');
 const projectCSS = document.getElementById('projectsCSS');
 const projectJS = document.getElementById('projectsJS');
-
+//Creating a new button element
 projectHTML.addEventListener('click', function(){
-    alert('Button clicked')
+   // Redirect to a different page
+   window.location.href = 'https://github.com/DrDevEli/HTML_Basics'; // Redirect to Git repo
 })

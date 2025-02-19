b78d155b9665d7c779abf893e48c14c582994c74
Milli <117689938+DrDevEli@users.noreply.github.com>
2025-02-19 18:45:12 +0100

    Rename variables and replace tipos

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 //Adding event listeners to the img from Portfolio 
-const projectHTML = document.getElementById('projectsHTML');
-const projectCSS = document.getElementById('projectsCSS');
-const projectJS = document.getElementById('projectsJS');
+const projectHTMLImg = document.getElementById('projectsHTML');
+const projectCSSImg = document.getElementById('projectsCSS');
+const projectJSImg = document.getElementById('projectsJS');
 //Creating a new button element
 projectHTML.addEventListener('click', function(){
     const HTMLbutton = document.createElement('button');
     HTMLbutton.style.cursor = 'pointer';
     //Replace the img with button
-    projectHTML.replaceWith(buttonElement);
+    projectHTMLImg.replaceWith(HTMLbutton);
     //Add an Event listener to the new button
     HTMLbutton.addEventListener('click', function(){
         alert('Button clicked!');

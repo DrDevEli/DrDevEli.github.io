3c55b64c46d1f0f6735014308832534770fab6f7
Milli <117689938+DrDevEli@users.noreply.github.com>
2025-02-28 19:18:04 +0100

    HTML button

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 //Adding event listeners to the img from Portfolio 
 const projectHTMLImg = document.getElementById('projectsHTML').addEventListener('click', function(){
     const HTMLbutton = document.createElement('button');
+    projectHTMLImg.appendChild(HTMLbutton);
     HTMLbutton.style.cursor = 'pointer';
     //Replace the img with button
-    projectHTMLImg.replaceWith(HTMLbutton);
+    // projectHTMLImg.replaceWith(HTMLbutton);
     //Add an Event listener to the new button
     HTMLbutton.addEventListener('click', function(){
         alert('Button clicked!');
